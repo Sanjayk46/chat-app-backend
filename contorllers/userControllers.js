fb274b29bb863b0f364ff58b210d824fcd518a9d
Sanjayk46 <sanjayks8046@gmail.com>
2024-09-21 22:08:19 +0530

    user changes added

---
+++ contorllers/userControllers.js
@@ -0,0 +1,186 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+const generateToken = require('../config/generateToken');
+const nodemailer = require('nodemailer');
+const bcrypt =require ('bcryptjs');
+const registerUser = asyncHandler(async (req,res) =>{
+   const {firstName,lastName, email, password, pic} = req.body;
+
+   if(!name || !email || !password){
+      res.status(400);
+      throw new Error('Please enter all the fields');
+   }
+
+   const userExists = await User.findOne({email});
+
+   if(userExists){
+      res.status(400);
+      throw new Error('User already exists');
+   }
+
+   const user = await User.create({
+      name,
+      email,
+      password,
+      pic
+   });
+
+   if(user){
+      res.status(201).json({
+         _id: user._id,
+         name: user.name,
+         email: user.email,
+         pic:user.pic,
+         token: generateToken(user._id)
+      })
+
+   }else{
+      res.status(400);
+      throw new Error('User not found');
+   }
+});
+
+const authUser = asyncHandler(async (req, res) =>{
+   const {email, password} = req.body;
+
+   const user = await User.findOne({email});
+
+   if(user && (await user.matchPassword(password))){
+      res.json({
+         _id: user._id,
+         name: user.name,
+         email: user.email,
+         pic:user.pic,
+         token: generateToken(user._id)
+      })
+   }else{
+      res.status(400);
+      throw new Error('Invalid email or password');
+   }
+})
+
+// .api/user
+const allUsers = asyncHandler(async (req,res)=>{
+   const keyword = req.query.search ? {
+      $or:[
+        {name:{$regex: req.query.search, $options:"i"}},
+        {email:{$regex: req.query.search, $options:"i"}}
+      ]
+   }:{
+
+   }
+   const users = await User.find(keyword).find({_id:{$ne:req.user._id}});
+   res.send(users);
+})
+
+const updateProfile = asyncHandler(async(req,res)=>{
+   const { name,email,address } = req.body;
+   const user = await User.findByIdAndUpdate(
+     req.user.id,
+     { firstName, lastName, address },
+     { new: true }
+   )
+});
+
+//forgot password
+
+const forgotPassword = asyncHandler(async(req,res)=>{
+   const { email } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const generateOTP = () => {
+      const characters = "0123456789";
+      return Array.from(
+        { length: 6 },
+        () => characters[Math.floor(Math.random() * characters.length)]
+      ).join("");
+    };
+
+    const OTP = generateOTP();
+    user.resetPasswordOtp = OTP;
+    user.resetPasswordExpires = Date.now() + 3600000;
+    await user.save();
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.USER_MAILER,
+        pass: process.env.USER_PASS,
+      },
+    });
+    //${user.firstName} ${user.lastName},
+    const mailOptions = {
+      from: "noreplychatapp2024@gmail.com",
+      to: user.email,
+      subject: "Password Reset",
+      html: `
+        <p>Dear ${user.firstName} ${user.lastName},</p>
+        <p>We received a request to reset your password. Here is your One-Time Password (OTP): <strong>${OTP}</strong></p>
+        <p>Please click the following link to reset your password:</p>
+        <a href="http://localhost:3000/reset-password">Reset Password</a>
+        <p>If you did not make this request, please ignore this email.</p>
+        <p>Thank you,</p>
+        <p>From Validation</p>
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Password reset email sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+})
+
+const resetPassword = asyncHandler(async(req,res)=>{
+   try {
+      const { OTP, password } = req.body;
+  
+      const user = await User.findOne({
+        resetPasswordOtp: OTP,
+        resetPasswordExpires: { $gt: Date.now() },
+      });
+  
+      if (!user) {
+        const message = user ? "OTP has expired" : "Invalid OTP";
+        return res.status(404).json({ message });
+      }
+      const expirationTime = Date.now() + (5 * 60 * 1000); // 5 minutes in milliseconds
+        
+      // Update the user's resetPasswordExpires field with the new expiration time
+      user.resetPasswordExpires = expirationTime;
+      const hashedPassword = await bcrypt.hash(
+        password,
+        PASSWORD_HASH_SALT_ROUNDS
+      );
+      user.password = hashedPassword;
+      user.resetPasswordOtp = null;
+      user.resetPasswordExpires = null;
+  
+      await user.save();
+  
+      res.status(200).json({ message: "Password changed successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+})
+const getUserProfile = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const selectedUser = await User.findOne({ _id: id }).select('-password');
+    res.status(200).json(selectedUser);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+
+
+module.exports = {registerUser, authUser, allUsers,updateProfile,forgotPassword,resetPassword,getUserProfile}
--- index.js
+++ index.js
@@ -1,32 +1,96 @@
 const express = require('express');
-const dotenv = require("dotenv");
-const cors = require ("cors");
-const {dbConnection} = require("./model/userModel");
-const userRouter = require("./Router/userRouter");
-const messageRouter = require('./Router/messageRouter');
-const chatRouter = require('./Router/chatRouter');
-const PORT=8001;
-const app = express();
+const dotenv = require('dotenv')
+const { chats } = require('./data/data');
+const {connectDB} = require('./config/db');
+const colors = require('colors');
+const cors = require('cors');
+const userRoutes = require('./Router/userRouter');
+const chatRoutes = require('./Router/chatRouter');
+const messageRoutes = require('./Router/messageRouter');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const path = require('path');
+const { PassThrough } = require('stream');
 
-app.use(cors());
-app.use(express.json());
 dotenv.config();
-dbConnection();
-app.get("/", (req, res) => {
-    try {
-      res.status(200).send({
-        message:"working"
-       });
-      
-    } catch (error) {
-      res.status(500).send({
-        message:"Internal Server Error",
-        error:error.message
+connectDB();
+const app = express();
+app.use(cors({ // Use cors as a function
+  origin: 'http://localhost:3000',
+ // origin:'https://food-app-skrestaurant.netlify.app',
+  //origin:'https://skrestaurant-food.netlify.app', 
+  optionsSuccessStatus: 200
+})); 
+
+app.use(express.json()); // to accept the json data
+
+
+
+app.use('/api/user', userRoutes)
+app.use('/api/chat', chatRoutes)
+app.use('/api/message', messageRoutes)
+
+// ----------- Deployment--------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'));
     })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running now');
+    })
+}
+
+// ----------- Deployment--------------
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: 'http://localhost:3000'
     }
-});
-app.use('/user',userRouter);
-app.use('/chat',chatRouter);
-app.use('/message',messageRouter);
-  app.listen(PORT,()=>console.log("app is running successfully"))
-    
+})
+
+io.on('connection', (socket) => {
+    console.log('connected to socket.io');
+    socket.on('setup', (userData) => {
+        socket.join(userData._id);
+        socket.emit('connected');
+    })
+
+    socket.on('join chat', (room) => {
+        socket.join(room);
+        console.log('User Joined Room: ' + room);
+
+    });
+
+    socket.on('typing', (room) => socket.in(room).emit('typing'));
+    socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+
+    socket.on('new message', (newMessageRecieved) => {
+        var chat = newMessageRecieved.chat;
+
+        if (!chat.users) return console.log('chat.users not defined');
+
+        chat.users.forEach(user => {
+            if (user._id == newMessageRecieved.sender._id) return;
+
+            socket.in(user._id).emit('message recieved', newMessageRecieved);
+        })
+    });
+
+    socket.off('setup', () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+    })
+
+})
